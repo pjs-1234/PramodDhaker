4ccaff198e01c356f37284b26a6b22100cffe7a0
Prajjwal Shrivastava <100458063+pjs-1234@users.noreply.github.com>
2025-09-15 15:36:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// Initialize AOS (Animate on Scroll)
+AOS.init({
+    duration: 1000,
+    once: true,
+});
+
+// Initialize Typed.js
+var typed = new Typed(".typed-text", {
+    strings: [
+        "Translating business needs into actionable solutions",
+        "Bridging business and technology",
+        "Creating value through analysis",
+    ],
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop: true,
+});
+
+// Activate scrollspy to highlight nav items on scroll
+const mainNav = document.body.querySelector("#mainNav");
+if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+        target: "#mainNav",
+        offset: 80,
+    });
+}
